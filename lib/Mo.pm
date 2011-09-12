52832b8b2cf715f1938657ad8dda510911b839ec
Ingy döt Net <ingy@cpan.org>
2011-09-12 15:20:07 +0000

    Import of INGY/Mo-0.22 from CPAN.
    gitpan-cpan-distribution: Mo
    gitpan-cpan-version:      0.22
    gitpan-cpan-path:         INGY/Mo-0.22.tar.gz
    gitpan-cpan-author:       INGY
    gitpan-cpan-maturity:     released

--- lib/Mo.pm
+++ lib/Mo.pm
@@ -1,2 +1,3 @@
-package Mo;$Mo::VERSION='0.21';
-require strict;require warnings;sub import{strict->import;warnings->import;my$p=caller;@{$p.'::ISA'}=$_[0];*{$p.'::extends'}=sub{@{(caller).'::ISA'}=$_[0];eval"require($_[0])"};*{$p.'::has'}=sub{my($n,%a)=@_;my($d,$b)=@a{qw(default builder)};*{(caller)."::$n"}=$d?sub{$#_?($_[0]{$n}=$_[1]):(exists$_[0]{$n})?$_[0]{$n}:($_[0]{$n}=$d->($_[0]))}:$b?sub{$#_?($_[0]{$n}=$_[1]):(exists$_[0]{$n})?$_[0]{$n}:($_[0]{$n}=$_[0]->$b)}:sub{$#_?$_[0]{$n}=$_[1]:$_[0]{$n}}}}sub new{my$c=shift;my$s=bless{@_},$c;my@c=$c;unshift@c,$c while$c=${$c."::ISA"}[0];do{&{$_."::BUILD"}($s)if defined&{$_."::BUILD"}}for@c;$s}
+package Mo;
+$VERSION='0.22';
+no warnings;sub import{$_->import for strict,warnings;my$p=caller."'";@{$p.ISA}=Mo::_;*{$p.extends}=sub{@{$p.ISA}=$_[0];eval"require $_[0]"};*{$p.has}=sub{my($n,%a)=@_;my$d=$a{default}||$a{builder};*{$p.$n}=$d?sub{$#_?($_[0]{$n}=$_[1]):exists$_[0]{$n}?$_[0]{$n}:($_[0]{$n}=$_[0]->$d)}:sub{$#_?$_[0]{$n}=$_[1]:$_[0]{$n}}}}sub Mo'_'new{my$c=shift;my$s=bless{@_},$c;my@c;do{unshift@c,$c."'BUILD"}while$c=${$c."'ISA"}[0];defined&$_&&&$_($s)for@c;$s}no strict
