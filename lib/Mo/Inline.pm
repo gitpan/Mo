d3bb21b1c091bc4207b438f3eb01cbaad8d305b2
Ingy döt Net <ingy@cpan.org>
2011-10-03 17:04:42 +0000

    Import of INGY/Mo-0.26 from CPAN.
    gitpan-cpan-distribution: Mo
    gitpan-cpan-version:      0.26
    gitpan-cpan-path:         INGY/Mo-0.26.tar.gz
    gitpan-cpan-author:       INGY
    gitpan-cpan-maturity:     released

--- lib/Mo/Inline.pm
+++ lib/Mo/Inline.pm
@@ -10,7 +10,7 @@
 package Mo::Inline;
 use Mo;
 
-our $VERSION = 0.25;
+our $VERSION = 0.26;
 
 use IO::All;
 
@@ -76,11 +76,10 @@ sub inliner {
         my @lines = io($INC{$module})->chomp->getlines;
         $lines[-1];
     } ('Mo', map { s!::!/!g; "Mo/$_" } @features);
-    $inline .= 'use strict;use warnings;';
     return <<"...";
 #   The following line of code was produced from the previous line by
 #   Mo::Inline version $VERSION
-$inline
+$inline\@f=qw[@features];use strict;use warnings;
 ...
 }
 
